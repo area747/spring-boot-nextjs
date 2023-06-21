e1049630c40d193f3cbab672bfa42e070ee584e8
softin-yusungho <yusungho@softin.co.kr>
2023-06-21 17:57:39 +0900

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { GetServerSideProps } from "next";
+
+export default function Page({initialData}) {
+
+    return <div>Hello, Next.js!
+        <div>
+          {initialData}
+        </div>
+    </div>
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    // 서버 측에서 초기 데이터를 가져옴
+    const response = await fetch('http://localhost:8080/api');
+    const initialData = await response.text();
+
+    return {
+      props: {
+        initialData,
+      },
+    };
+}
+
+// const getApi = async () =>{
+//     const res = await fetch('http://localhost:8080/api', { cache: 'force-cache' });
+//     const resText = await res.text();
+
+//     return resText;
+// }
